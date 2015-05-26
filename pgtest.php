a2e351bd6ced448cdf60493facbcf9fd0129612a
iscalejandrogr <i.s.c.alejandro.gr@gmail.com>
2015-05-26 14:55:09 -0500

    esto es un comentario8

--- pgtest.php
+++ pgtest.php
@@ -1,18 +1,18 @@
 <?php
 //comprobamos si ha ocurrido un error.
-if ($_FILES["imagen"]["error"] > 0){
+if ($_FILES["archivo"]["error"] > 0){
 	echo "ha ocurrido un error";
 } else {
-	//ahora vamos a verificar si el tipo de archivo es un tipo de imagen permitido.
+	//ahora vamos a verificar si el tipo de archivo es un tipo de archivo permitido.
 	//y que el tamano del archivo no exceda los 100kb
 	$permitidos = array("image/jpg", "image/jpeg", "image/gif", "image/png");
 	$limite_kb = 10000;
 
-	if (in_array($_FILES['imagen']['type'], $permitidos)){
-		//esta es la ruta donde copiaremos la imagen
+	if (in_array($_FILES['archivo']['type'], $permitidos)){
+		//esta es la ruta donde copiaremos la archivo
 		//recuerden que deben crear un directorio con este mismo nombre
 		//en el mismo lugar donde se encuentra el archivo subir.php
-		$ruta = "imagenes/" . $_FILES['imagen']['name'];
+		$ruta = "imagenes/" . $_FILES['archivo']['name'];
 		//comprovamos si este archivo existe para no volverlo a copiar.
 		//pero si quieren pueden obviar esto si no es necesario.
 		//o pueden darle otro nombre para que no sobreescriba el actual.
@@ -20,14 +20,14 @@ if ($_FILES["imagen"]["error"] > 0){
 			//aqui movemos el archivo desde la ruta temporal a nuestra ruta
 			//usamos la variable $resultado para almacenar el resultado del proceso de mover el archivo
 			//almacenara true o false
-			$resultado = @move_uploaded_file($_FILES["imagen"]["tmp_name"], $ruta);
+			$resultado = @move_uploaded_file($_FILES["archivo"]["tmp_name"], $ruta);
 			if ($resultado){
 				echo "el archivo ha sido movido exitosamente";
 			} else {
 				echo "ocurrio un error al mover el archivo.";
 			}
 		} else {
-			echo $_FILES['imagen']['name'] . ", este archivo existe";
+			echo $_FILES['archivo']['name'] . ", este archivo existe";
 		}
 	} else {
 		echo "archivo no permitido, es tipo de archivo prohibido o excede el tamano de $limite_kb Kilobytes";
